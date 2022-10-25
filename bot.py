f538d9fa070e81aee9c6a5cde5162a439a4746d7
Eoghan <eoghanmccarroll@gmail.com>
2022-10-24 20:49:42 -0400

    Added new conch response. Adjusted response weight to factor in new response.

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from ast import If
 import discord
 import random
 from decouple import config
@@ -7,14 +8,18 @@ TOKEN =  config('DISCORD_API_KEY')
 intents = discord.Intents.default()
 intents.message_content = True
 
-responses = ['yes','no','maybe someday','try asking again']
+responses = ['yes','no','try asking again'] #1/3 Equal chance of yes, no and re-roll
+negativeResponses= ['no','no','maybe someday',"I don't think so"] # 50% chance default no, 25% each alternate
 
 client = discord.Client(intents = intents)
 
 def conch_decide(message_string):
     print(message_string)
     try:
-        return random.choice(responses)
+        choice = random.choice(responses)
+        if choice == 'no':
+            choice = random.choice(negativeResponses)
+        return choice
     except:
         return "Try asking again"
 
